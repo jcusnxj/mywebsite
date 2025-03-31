cda75994fa63f0678d8498f2e88f4b7028c50f9e
jcusnxjnet <frantisek@jcusnxj.net>
2025-03-31 17:11:12 +0200

    initial commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,6 @@
 import 'dotenv/config'
 import { DateTime } from "luxon";
-import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
-import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 export default function (eleventyConfig) {
 
@@ -12,28 +10,8 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("views/assets/js");
 
     // PLUGINS
-    eleventyConfig.addPlugin(syntaxHighlight);
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-    // rss
-    eleventyConfig.addPlugin(feedPlugin, {
-		type: "atom", // or "rss", "json"
-		outputPath: "/feed.xml",
-		collection: {
-			name: "post", // iterate over `collections.posts`
-			limit: 10,     // 0 means no limit
-		},
-		metadata: {
-			language: "cs",
-			title: "80fk.net",
-			subtitle: "Osobní stránky vytvořené v 11ty.",
-			base: "https://80fk.net/",
-			author: {
-				name: "František",
-				email: "", // Optional
-			}
-		}
-	});
     
     // FILTERS
     // Luxon date filter (for JavaScript Date object)
